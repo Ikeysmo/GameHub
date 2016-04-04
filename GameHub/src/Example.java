3463da2bdc67b35346a9b2ff8084c43e90c35f38
Itsmoak <Itsmoak@ncsu.edu>
2016-04-04 12:31:54 -0400

    Bringing it online

---
+++ GameHub/src/Example.java
@@ -0,0 +1,13 @@
+//just here to configure github!
+public class Example {
+
+	public Example() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
