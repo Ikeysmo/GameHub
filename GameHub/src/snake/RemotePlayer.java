05b0b6e254846b31365cf44496689ee15b0b6294
zacharyjones123 <zrjones@ncsu.edu>
2016-05-10 16:49:44 -0700

    Javadoc and Comment RemotePlayer.java

--- GameHub/src/snake/RemotePlayer.java
+++ GameHub/src/snake/RemotePlayer.java
@@ -1,5 +1,13 @@
 package snake;
 
+/**
+ * This is the Remote Player class for the Snake Game
+ * 
+ * @author Zachary Jones
+ * @author Isaiah Smoak
+ * @version 1.0
+ */
+
 import java.awt.Point;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -10,27 +18,41 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class RemotePlayer extends SnakePlayer implements Runnable {
+	/*Port the remotePlayer is running on*/
 	int port = 2000;
+	/*IP address of the remote player*/
 	String ip_address;
+	/*Socket for the remote player*/
 	Socket s;
+	/* For retrieving objects*/
 	ObjectInputStream ois;
+	/* For sending objects*/
 	ObjectOutputStream oos;
-	public Snake tic;
+	/* The Snake games the remote player is running on*/
+	public Snake snake;
 	
-	public RemotePlayer(String ipAddress, Snake tic, SnakePlayer op) {
-		this.tic = tic;
+	/**
+	 * The constructor of the RemotePlayer
+	 * 
+	 * @param ipAddress IP addres of the player
+	 * @param tic The game snake
+	 * @param op TheSnakePlayer for this RemotePLayer
+	 */
+	public RemotePlayer(String ipaddress, Snake snake, SnakePlayer op) {
+		this.snake = snake;
+		this.ip_address = ipaddress;
 		
-		ip_address = ipAddress;
 		try {
-			s = new Socket();
-			s.connect(new InetSocketAddress(ip_address, port), 500);
-			oos = new ObjectOutputStream(s.getOutputStream());
-			ois = new ObjectInputStream(s.getInputStream());
-			oos.writeObject(op.name);
-			name = (String) ois.readObject();
-			tic.turn = 1;
-			piece = 'O';
-			//player 2	
+			s = new Socket(); //Make a new socket
+			s.connect(new InetSocketAddress(ip_address, port), 500); //Open the new Socket
+			oos = new ObjectOutputStream(s.getOutputStream()); //Make a object output stream
+			ois = new ObjectInputStream(s.getInputStream()); //Make a object input stream
+			oos.writeObject(op.name); //Send the SnakePlayer to the object output stream
+			name = (String) ois.readObject(); //Read the name of the player from the object input stream
+			snake.turn = 1; //Set whose turn it is in the game
+			piece = 'O'; //Give the player a piece
+			//player 2
+			//TODO: You need to comment out how all of the exceptions work
 		}
 		catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
@@ -39,7 +61,7 @@ public class RemotePlayer extends SnakePlayer implements Runnable {
 			// TODO Auto-generated catch block
 			System.out.println("Couldn't connect.. beginning server side!");
 			try {//if failed, try hosting! and become player 1
-				tic.turn = 0;
+				snake.turn = 0;
 				piece = 'X';
 				ServerSocket ss = new ServerSocket(port);
 				s = ss.accept();
@@ -67,46 +89,60 @@ public class RemotePlayer extends SnakePlayer implements Runnable {
 		
 	}
 
+	/**
+	 * Making a move as a RemotePlayer
+	 * 
+	 * @return the place chosen on the move
+	 */
 	@Override
 	public Point makeMove() throws IOException {
-		// TODO Auto-generated method stub
 		try {
-			Point answer;
+			Point answer; //Where the person made his move
+			
+			//This loop breaks when a valid move is found
 			while(true){
-				 answer = (Point)ois.readObject();
-				 if(tic == null)
-					 System.out.println("why");
+				 answer = (Point)ois.readObject(); //Look for the answer
+				 if(snake == null)
+					 System.out.println("There is no Snake game");
 				 else if(answer == null){
-					 System.out.println("bugg");
+					 System.out.println("There is not in the Object input stream");
 				 }
-				 if(tic.updateMove(answer.x, answer.y, this)) //if this is true, valid move!
+				 if(snake.updateMove(answer.x, answer.y, this)) //if this is true, valid move!
 					break;
 			}
 			return answer;
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
 	}
 	
+	/**
+	 * This method updates RemotePLayer with the move made
+	 * and any other changes
+	 * 
+	 * @param p The point
+	 */
 	public void updateAll(Point p) throws IOException{
+		//TODO: What does this mean?
 		//update everything
 		oos.writeObject(p);
 		
 	}
 
-	
+	/**
+	 * This is the run method, where the move is done
+	 * 
+	 */
 	public void run(){
-		//recieving...
 		Point answer;
+		
+		//This loop breaks when a move is found
 		while(true){
 			try {
 				answer = (Point) ois.readObject();
-				System.out.println(answer);
 			} catch (ClassNotFoundException | IOException e) {
-				// TODO Auto-generated catch block
-				//e.printStackTrace();
+				e.printStackTrace();
 			}
 		}
 	}
