9228fd923d72a82ffee29f3e7e58db40a3843e74
zacharyjones123 <zrjones@ncsu.edu>
2016-05-11 15:52:31 -0700

    Javadoc and Comment ConnectFourPlayer.java

--- GameHub/src/connectFour/ConnectFourPlayer.java
+++ GameHub/src/connectFour/ConnectFourPlayer.java
@@ -1,5 +1,13 @@
 package connectFour;
 
+/**
+ * The player for Connect Four
+ * 
+ * @author Zachary Jones
+ * @author Isaiah Smoak
+ * @version 1.0
+ */
+
 import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -8,18 +16,26 @@ import java.io.IOException;
 import gameHub.Player;
 
 public class ConnectFourPlayer extends Player implements MouseListener {
+	/* Is the mouse clicked? */
 	public boolean mouseNotClicked = false;
+	/* Is it ready?*/
 	public boolean ready = false;
+	/* The board being played on*/
 	private BoardPanel panel = null;
-	protected ConnectFour connectFour = null;
+	/* The TicTacToe game*/
+	protected TicTacToe tic = null;
+	/* X-cord of the move */
 	protected int numx = -1;
+	/* Y-cord of the move */
 	protected int numy = -1;
+	/* Piece of this player*/
 	protected char piece;
-
-	public ConnectFourPlayer() {
-		
-	}
 	
+	/**
+	 * Makes the move for the player
+	 * 
+	 * @return The point of the move made
+	 */
 	public synchronized Point makeMove() throws IOException {
 		
 		while(true) {
@@ -36,11 +52,16 @@ public class ConnectFourPlayer extends Player implements MouseListener {
 	}
 		//wait for event...
 	
+	/**
+	 * Method for if a mouse is pressed
+	 * 
+	 * @param e The mouse being pressed
+	 */
 	@Override
 	public synchronized void mousePressed(MouseEvent e) {
 		
-		int widthFactor = panel.getWidth()/10;
-		int heightFactor = panel.getHeight()/10;
+		int widthFactor = panel.getWidth()/COLUMNSNUM;
+		int heightFactor = panel.getHeight()/ROWSNUM;
 		numx = e.getX()/widthFactor;
 		numy = e.getY()/heightFactor;
 		
@@ -49,14 +70,29 @@ public class ConnectFourPlayer extends Player implements MouseListener {
 		panel.removeMouseListener(this);
 	}
 	
+	/**
+	 * Getter method for the name of the player
+	 * 
+	 * @return the name of the player
+	 */
 	@Override
 	public void mouseClicked(MouseEvent e) {
 	}
 	
+	/**
+	 * Getter method for the name of the player
+	 * 
+	 * @return the name of the player
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Assigns a piece to the player (Setter)
+	 * 
+	 * @param 'X' or 'O'
+	 */
 	public void assignPiece(char x) {
 		if(x == 'X' || x == 'O')
 			this.piece = x;
@@ -64,36 +100,39 @@ public class ConnectFourPlayer extends Player implements MouseListener {
 			throw new IllegalArgumentException();
 	}
 	
+	/**
+	 * Getter method for the piece
+	 * 
+	 * @return the piece of the player
+	 */
 	public char getPiece() {
 		return this.piece;
 	}
 	
+	/**
+	 * Getter method for the name of the player
+	 * 
+	 * @param name The name of the player
+	 */
 	public ConnectFourPlayer(String name) {
 		this.name = name;
 	}
 	
+	/**
+	 * Getter method for the board
+	 * 
+	 * @return the board
+	 */
 	public void getPanel(BoardPanel panel) {
 		this.panel = panel;
 	}
 	
+	/**
+	 * Getter method for the Connect Four game
+	 * 
+	 * @return the Connect Four game
+	 */
 	public void getConnectFour(ConnectFour connectFour) {
 		this.connectFour = connectFour;
-	}
-	
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
 }
