98c2db9fa3c4b4afb10eb3bba403270ac1264f79
zacharyjones123 <zrjones@ncsu.edu>
2016-05-11 14:52:06 -0700

    Javadoc and Comment TicTacToePlayer.java

--- GameHub/src/ticTacToe/TicTacToePlayer.java
+++ GameHub/src/ticTacToe/TicTacToePlayer.java
@@ -1,5 +1,12 @@
 package ticTacToe;
 
+/**
+ * The player of the TicTacToe game
+ * 
+ * @author Zachary Jones
+ * @author Isaiah Smoak
+ */
+
 import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -10,29 +17,43 @@ import ticTacToe.BoardPanel;
 import ticTacToe.TicTacToe;
 
 public class TicTacToePlayer extends Player implements MouseListener{
+	/* Is the mouse clicked? */
 	public boolean mouseNotClicked = false;
+	/* Is it ready?*/
 	public boolean ready = false;
+	/* The board being played on*/
 	private BoardPanel panel = null;
+	/* The TicTacToe game*/
 	protected TicTacToe tic = null;
+	/* X-cord of the move */
 	protected int numx = -1;
+	/* Y-cord of the move */
 	protected int numy = -1;
+	/* Piece of this player*/
 	protected char piece;
+	/* Name of the player*/
 	protected String name;
 	
-	public TicTacToePlayer(){}
+	/**
+	 * Constructor of TicTacToePlayer
+	 * 
+	 * @param name The name of the player
+	 */
 	public TicTacToePlayer(String name){
 		this.name = name;
 	}
 	
-	//somehow get move?
+	/**
+	 * Makes the move for the player
+	 * 
+	 * @return The point of the move made
+	 */
 	public synchronized Point makeMove() throws IOException {
-		// TODO Auto-generated method stub
 		while(true){
 			try {
 				panel.addMouseListener(this);
 				wait();
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			if(tic.updateMove(numx, numy, this)) //if this is true, valid move!
@@ -40,14 +61,18 @@ public class TicTacToePlayer extends Player implements MouseListener{
 		}
 		return new Point(numx,numy);
 	}
-		//wait for event... 
 	
+	/**
+	 * Method for if a mouse is pressed
+	 * 
+	 * @param e The mouse being pressed
+	 */
 	@Override
 	public synchronized void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
-		int widthFactor = panel.getWidth()/3;
-		int heightFactor = panel.getHeight()/3;
+		int widthFactor = panel.getWidth()/COLUMNSNUM;
+		int heightFactor = panel.getHeight()/ROWSNUM;
 		numx = e.getX()/widthFactor;
 		numy = e.getY()/heightFactor;
 		
@@ -56,49 +81,52 @@ public class TicTacToePlayer extends Player implements MouseListener{
 		panel.removeMouseListener(this);
 	}
 	
-	
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	
+	/**
+	 * Getter method for the name of the player
+	 * 
+	 * @return the name of the player
+	 */
 	public String getName(){
 		return name;
 	}
+	
+	/**
+	 * Assigns a piece to the player (Setter)
+	 * 
+	 * @param 'X' or 'O'
+	 */
 	public void assignPiece(char x){
 		if(x == 'X' || x == 'O')
 			this.piece = x; //assign piece if this piece is legal!
 		else
 			throw new IllegalArgumentException();
 	}
+	
+	/**
+	 * Getter method for the piece
+	 * 
+	 * @return the piece of the player
+	 */
 	public char getPiece(){
 		return this.piece;
 	}
 	
+	/**
+	 * Getter method for the board
+	 * 
+	 * @return the board
+	 */
 	public void getPanel(BoardPanel panel){
 		this.panel = panel;
 	}
+	
+	/**
+	 * Getter method for the TicTacToe game
+	 * 
+	 * @return the TicTacToe game
+	 */
 	public void getTic(TicTacToe tic){
 		this.tic = tic;
 	}
-	
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
 
 }
