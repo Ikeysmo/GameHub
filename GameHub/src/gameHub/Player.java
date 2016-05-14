0f99eab6c6b9a482307bc9095b96171899245c17
zacharyjones123 <zrjones@ncsu.edu>
2016-05-14 18:42:53 -0400

    Update Player.java
    asdf

--- GameHub/src/gameHub/Player.java
+++ GameHub/src/gameHub/Player.java
@@ -1,27 +1,56 @@
 package gameHub;
 
+/**
+ * The abstract class for the Player
+ * 
+ * @author Isaiah Smoak
+ * @author Zachary Jones
+ */
+
 import java.awt.Point;
 import java.io.IOException;
 
 public class Player {
+	/* The name of the player */
 	public String name;
 	
+	/**
+	 * Constructor of the Player class
+	 */
 	public Player() {
-		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * Make move method for the players
+	 * 
+	 * @return The point where the move was made
+	 * @throws IOException
+	 */
 	public Point makeMove() throws IOException {
 		return null;
 	}
 	
+	/**
+	 * Shows the score of the player
+	 */
 	public void showScore() {
 		
 	}
 	
+	/**
+	 * Getter method for the name
+	 * 
+	 * @return name
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Setter method for the name
+	 * 
+	 * @param name The name of the player
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
