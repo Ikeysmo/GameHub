c428707ae8628e283d9d8263c37c6702ff979ce3
zacharyjones123 <zrjones@ncsu.edu>
2016-05-14 18:43:12 -0400

    Update PlayerAccount.java
    asdf

--- GameHub/src/gameHub/PlayerAccount.java
+++ GameHub/src/gameHub/PlayerAccount.java
@@ -1,26 +1,58 @@
 package gameHub;
+
+/**
+ * This class is for the Player Account
+ * 
+ * @author Isaiah Smoak
+ * @author Zachary Jones
+ * @version 1.0
+ */
+
 import java.net.ConnectException;
 
 public class PlayerAccount {
+	/* The username of the player*/
 	String username;
+	/* The password of the player*/
 	String password;
-	Player temp; //hold reference to all the different type of players for instance games.
+	/* Hold reference to all the different type of players for instance games.*/
+	Player temp;
 	
+	/**
+	 * Constructor of Player Account
+	 * 
+	 * @param username The username of the player
+	 * @param password The password of the player
+	 */
 	public PlayerAccount(String username, String password) {
-		// TODO Auto-generated constructor stub
 		this.username = username;
 		this.password = password;
 	}
 
+	/**
+	 * This method trys to get online
+	 * Throw a connection if fails... then sign offline!
+	 * 
+	 * @throws ConnectException
+	 */
 	public void getOnline() throws ConnectException{
-		//throw a connection if fails... then sign offline!
 	}
 	
+	/**
+	 * Getter method for the score
+	 * 
+	 * @return score
+	 */
 	public int getScore(){
 		return 0;
 		
 	}
 	
+	/**
+	 * Getter method for the username
+	 * 
+	 * @return the username
+	 */
 	public String getUsername(){
 		return username;
 	}
